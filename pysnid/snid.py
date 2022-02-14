bbfc2b9641d68d118b8195ffe47b439e8cd1b78a
MickaelRigault <mick.rigault@gmail.com>
2022-02-14 13:27:02 +0100

    test

--- pysnid/snid.py
+++ pysnid/snid.py
@@ -434,7 +434,7 @@ class SNIDReader( object ):
         # - Data
         data_ = self.get_inputdata(fluxcorr=fluxcorr)
         if offset_coef is None:
-            offset_coef = 0#np.percentile(data_["flux"], )
+            offset_coef = np.percentile(data_["flux"], 50)
         elif offset_coef == "None":
             offset_coef = 0
 
